3aa18260d0c490c2ac5d13047ec932dbdac81f51
huseynManaf <huseynim@code.edu.az>
2026-09-07 14:05:15 +0400

    BakuServis remote WebView architecture and configs added

--- android/app/src/main/java/com/huguservis/app/MainActivity.java
+++ android/app/src/main/java/com/huguservis/app/MainActivity.java
@@ -1,5 +1,26 @@
 package com.bakuservis.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		WebView webView = getBridge().getWebView();
+		WebSettings settings = webView.getSettings();
+		settings.setJavaScriptEnabled(true);
+		settings.setDomStorageEnabled(true);
+		settings.setDatabaseEnabled(false);
+		settings.setAllowFileAccess(false);
+		settings.setAllowContentAccess(false);
+		settings.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
+		settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+		settings.setSupportZoom(false);
+		webView.clearCache(true);
+	}
+}
